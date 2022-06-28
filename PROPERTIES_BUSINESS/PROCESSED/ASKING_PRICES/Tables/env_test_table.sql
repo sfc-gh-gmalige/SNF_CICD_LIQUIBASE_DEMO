cda90a0f28418f58c8e76ba853a7a32c069be24e
gmalige <gokula.malige@snowflake.com>
2022-06-28 15:45:07 +0100

    Test

--- PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table.sql
+++ PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table.sql
@@ -2,5 +2,5 @@
 
 -- changeset GMALIGE:1 
  
-CREATE or repalce TABLE DEMO.test (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id));
+CREATE or repalce TABLE DEMO.test (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id))
  
