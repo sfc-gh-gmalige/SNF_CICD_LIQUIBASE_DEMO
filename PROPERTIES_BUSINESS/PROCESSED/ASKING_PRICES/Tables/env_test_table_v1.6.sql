fe92056361652ced1ef92615fd8090423111bcdb
gmalige <gokula.malige@snowflake.com>
2022-06-29 12:36:13 +0100

    Adding 6th table

---
+++ PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table_v1.6.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset GMALIGE:1 
+ 
+CREATE or replace TABLE DEMO.test_5 (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id));
+
+-- changeset GMALIGE:2 
+ 
+CREATE or replace TABLE DEMO.test_6 (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id));
+  
