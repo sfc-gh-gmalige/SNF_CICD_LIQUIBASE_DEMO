61fcc8586a48f0071dc6fb530789de953c6d7e65
gmalige <gokula.malige@snowflake.com>
2022-06-29 13:04:52 +0100

    QA first commit

--- PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table copy 2.sql
+++ PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table copy 2.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
 -- changeset GMALIGE:1 
-CREATE TABLE DEMO1.test3 (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id));
+CREATE TABLE DEMO.test3 (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id));
--- PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table copy.sql
+++ PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table copy.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
 -- changeset GMALIGE:1 
-CREATE TABLE DEMO1.test2 (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id));
+CREATE TABLE DEMO.test2 (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id));
